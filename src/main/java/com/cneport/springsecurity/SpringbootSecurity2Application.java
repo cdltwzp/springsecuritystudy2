c51241328ba84c2135079139370c23af50682d24
wzp <wzpcdlt@163.com>
2021-09-20 22:36:12 +0800

    注解权限控制

--- src/main/java/com/cneport/springsecurity/SpringbootSecurity2Application.java
+++ src/main/java/com/cneport/springsecurity/SpringbootSecurity2Application.java
@@ -2,8 +2,11 @@ package com.cneport.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+//@EnableGlobalMethodSecurity源码里面全部默认是false
+@EnableGlobalMethodSecurity(securedEnabled=true,prePostEnabled=true)//开启访问控制注解，如果不开启则无法使用注解，当未登陆时会让登陆，登陆完之后如果未匹配权限不是403，而是500 type=Internal Server Error, status=500
 public class SpringbootSecurity2Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/cneport/springsecurity/web/IndexControl.java
+++ src/main/java/com/cneport/springsecurity/web/IndexControl.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,7 +22,9 @@ public class IndexControl {
 	public @ResponseBody String login() {
 		return "redirect:main.html";
 	}
-	
+	//@Secured("ROLE_abc")//(27)有ROLE_abc角色才可以访问,注意这里的角色必须加上ROLE_前缀
+	//(28)@Secured是专门用于判断是否具有角色的，能写在方法或类上，参数要以ROLE_前缀
+	@PreAuthorize("hasAuthority(\"admin\")")//(29)方法前判断权限，PostAuthorize执行后判断权限 且EnableGlobalMethodSecurity必须开启prePostEnable=true 参数是access表达式
 	@RequestMapping(value="/toMain")//没有指定method属性时 get/post/puth/delete都可以访问
 	public String toMain() {
 		return "redirect:main.html";
